51ecc0cde6ae45455fd79a7c2033c5a9670e3aa1
Wwazam <basile.pracca@gmail.com>
2021-10-11 20:50:42 +0200

    Yahouuu

--- custom.el
+++ custom.el
@@ -4,7 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(evilem-style 'pre)
- '(package-selected-packages '(vimrc-mode ayu-theme lsp-pyright)))
+ '(jenkins-url "http://localhost:8080")
+ '(package-selected-packages
+   '(jenkinsfile-mode jenkins vagrant vimrc-mode ayu-theme lsp-pyright)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- packages.el
+++ packages.el
@@ -11,3 +11,5 @@
 
 (package! transpose-frame)
 (package! lsp-pyright)
+
+(package! jenkinsfile-mode)
