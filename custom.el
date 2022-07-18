ab6139c4d060def83bcd4d4f1201a71caa8ca326
Basile <basile.pracca@consultants-solutec.fr>
2022-07-18 10:03:47 +0200

    Add stuff

--- custom.el
+++ custom.el
@@ -21,24 +21,7 @@
      (elisp-lint-ignored-validators "byte-compile" "package-lint")))
  '(jenkins-url "http://localhost:8080")
  '(package-selected-packages
-   '(symex blamer bluetooth disk arduino-mode disk-usage puppet-mode org-sidebar kubedoc kubernetes-evil kubel-evil kubernetes jenkinsfile-mode jenkins vagrant vimrc-mode ayu-theme lsp-pyright))
- '(safe-local-variable-values
-   '((elisp-lint-indent-specs
-      (describe . 1)
-      (it . 1)
-      (thread-first . 0)
-      (cl-flet . 1)
-      (cl-flet* . 1)
-      (org-element-map . defun)
-      (org-roam-dolist-with-progress . 2)
-      (org-roam-with-temp-buffer . 1)
-      (org-with-point-at . 1)
-      (magit-insert-section . defun)
-      (magit-section-case . 0)
-      (org-roam-with-file . 2))
-     (elisp-lint-ignored-validators "byte-compile" "package-lint")
-     (cider-shadow-cljs-default-options . "app")))
- '(warning-suppress-log-types '((org-ql-select))))
+   '(topspace arduino-mode disk-usage puppet-mode org-sidebar kubedoc kubernetes-evil kubel-evil kubernetes jenkinsfile-mode jenkins vagrant vimrc-mode ayu-theme lsp-pyright)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -185,7 +185,7 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       (yaml +lsp)
+       (yaml)
        web               ; the tubes
 
        :email
